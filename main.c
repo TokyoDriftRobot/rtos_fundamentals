ea0da736eb0d6dc9c8cc6f5acc62e730bc0d04a0
nwjbrandon <nwjbrandon@gmail.com>
2020-09-10 12:25:59 +0800

    change from timer interrupt to pwm

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
 #include <MKL25Z4.H>
 #include "led_service.h"
-
+#define PTB0_Pin 0
+#define PTB1_Pin 1
 int main (void) {
 	 /* Disable global IRQs */
 	__disable_irq();       
 
-	led_init_gpio();
-	led_color = led_control(LED_BLUE_COLOR);
+	/* Enable clock on PTB */
+	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
+	
+	/* Select PTB0 to connect to TPM */
+	PORTB->PCR[PTB0_Pin] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[PTB0_Pin] |= PORT_PCR_MUX(3);
+	
+	/* Select PTB1 to connect to TPM */
+	PORTB->PCR[PTB1_Pin] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[PTB1_Pin] |= PORT_PCR_MUX(3);	
 	
 	/* Select MCGFLLCLK as timer counter clock */
 	SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
@@ -24,24 +33,20 @@ int main (void) {
 	TPM1->SC |= TPM_SC_CMOD(1);
 	
 	/* Select half of max modulo value */
-	TPM1->MOD = 0x7FFF;       
+	TPM1->MOD = 0x7FFF;               
 	
-	/* Enable timer overflow interrupt */
-	TPM1->SC |= TPM_SC_TOIE(1);         
-	
-//	/* Select counter to operate in up counting mode */
-//	TPM1->SC &= ~(TPM_SC_CPWMS_MASK);
-//	
-//	TPM1_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
-//	TPM1_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
-//	
-	/* Enable IRQ18 for TPM1 */
-	NVIC_SetPriority(TPM1_IRQn, 0);
-	NVIC_ClearPendingIRQ(TPM1_IRQn);
-	NVIC_EnableIRQ(TPM1_IRQn); // ALT: NVIC->ISER[0] |= 0x00040000; // enable IRQ18 (bit 18 of ISER[0])
+	/* Select counter to operate in up counting mode */
+	TPM1->SC &= ~(TPM_SC_CPWMS_MASK);
+
+  /* Set timer to edge-aligned pwm mode */
+	TPM1_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM1_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
 	
 	/* Enable global IRQs */
-	__enable_irq();         
+	__enable_irq();  
+	
+	TPM1_C0V = 0x0EA6;
+	TPM1_C1V = 0x753;
 
 	while (1);
 }
@@ -49,5 +54,6 @@ int main (void) {
 void TPM1_IRQHandler(void) {
 	led_color = led_control(led_color);
 	/* Clear TOF */
-	TPM1->SC |= TPM_SC_TOF(1);  
+//	TPM1->SC |= TPM_SC_TOF(1);
+  TPM1_C0SC |= TPM_CnSC_CHF(1);  
 }
